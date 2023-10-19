ff2c8447284d1bad0329f329a05b8deec56d50fb
Avavery <juliya.belka.19@yandex.ru>
2023-10-20 01:09:43 +0300

    add fonts and packages

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import styles from './App.module.css'
+
+const App: FC = () => {
+
+  return (
+    <>
+      <h1 className={styles.app}>Team chanel №5</h1>
+    </>
+  )
+}
+
+export default App
